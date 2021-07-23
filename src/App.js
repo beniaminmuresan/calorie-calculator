c416277d320f2505be38ebb273721e814b9680c4
Beniamin Muresan <muresan.beniamin@hotmail.com>
2021-07-23 18:41:54 +0300

    Add basic calorie calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [quantity, setQuantity] = useState(0);
+  const [caloriesPer100, setCaloriesPer100] = useState(0);
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{paddingTop: '20em'}}>
+      <div className="field"> 
+        <label>Cantitate (gr): </label>
+        <input placeholder = "Cantitate (g)" type = "text" onChange = { (e) => setQuantity(e.target.value) } value={quantity} />
+      </div>
+
+      <div className="field"> 
+        <label>Calorii/100(g) </label>
+        <input type = "text" onChange = { (e) => setCaloriesPer100(e.target.value) } value={caloriesPer100} />
+      </div>
+      <div className="field" style={{paddingTop: '20px'}}>
+        <button onClick={() => setCount( quantity * caloriesPer100 / 100 )}> 
+          Calculate
+        </button>
+      </div>
+
+      <h3>Rezultat calorii: {count}</h3>
     </div>
   );
 }
